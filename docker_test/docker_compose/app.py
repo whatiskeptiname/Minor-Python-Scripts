585cfb8a560c2adce483c55dc0f5c2fd72ac8148
whatiskeptiname <susangmree@gmail.com>
2022-04-19 11:16:34 +0545

    implemented caching

--- docker_test/docker_compose/app.py
+++ docker_test/docker_compose/app.py
@@ -1,23 +1,18 @@
-import redis
-from flask import Flask, session
+import requests
+from flask import Flask, jsonify, request
+from flask_caching import Cache  # Import Cache from flask_caching module
 
 app = Flask(__name__)
-
-app.secret_key = "GENERATE_A_SECRET_THEN_PLACE_HERE"
-
-# Configure Redis for storing the session data on the server-side
-app.config["SESSION_TYPE"] = "redis"
-app.config["SESSION_REDIS"] = redis.from_url("redis://localhost:6379")
+app.config.from_object(
+    "config.Config"
+)  # Set the configuration variables to the flask application
+cache = Cache(app)  # Initialize Cache
 
 
-@app.route("/")
-def hello():
-    if "code" in session:
-        return "Hello %s!" % session["code"]
-    session["code"] = "again!!!"
-
-    return "Hello"
-
-
-if __name__ == "__main__":
-    app.run()
+@app.route("/universities")
+@cache.cached(timeout=30, query_string=True)
+def get_universities():
+    API_URL = "http://universities.hipolabs.com/search?country="
+    search = request.args.get("country")
+    r = requests.get(f"{API_URL}{search}")
+    return jsonify(r.json())
