aab071ae988323ca1b4c190e7dfc0c0caa0c4707
whatiskeptiname <susangmree@gmail.com>
2022-04-22 13:40:29 +0545

    added args in thread

--- threading_practice/main.py
+++ threading_practice/main.py
@@ -5,9 +5,9 @@ import time
 start = time.perf_counter()
 
 
-def do_something():
-    print("Sleeping 1 second...")
-    time.sleep(1)
+def do_something(seconds):
+    print(f"Sleeping {seconds} second(s)...")
+    time.sleep(seconds)
     print("Done Sleeping...")
 
 
@@ -16,7 +16,7 @@ threads = []
 
 # Create multiple threads
 for _ in range(10):
-    t = threading.Thread(target=do_something)
+    t = threading.Thread(target=do_something, args=[1.5])
     t.start()
     threads.append(t)
 
