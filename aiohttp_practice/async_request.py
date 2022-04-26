4bd1473f6d66c1baaed5403671b22642b7c3f28f
whatiskeptiname <susangmree@gmail.com>
2022-04-26 17:39:23 +0545

    basic async aiohttp

--- aiohttp_practice/async_request.py
+++ aiohttp_practice/async_request.py
@@ -1,21 +1,30 @@
 import asyncio
+from asyncio import tasks
 import aiohttp
-import requests
 import os
 
-# To work with the .env file
-from dotenv import load_dotenv
-
-load_dotenv()
 
 api_key = os.getenv("ALPHAVANTAGE_API_KEY")
 url = "https://www.alphavantage.co/query?function=OVERVIEW&symbol={}&apikey={}"
 symbols = ["AAPL", "GOOG", "TSLA", "MSFT", "PEP", "AAPL", "GOOG", "TSLA", "MSFT", "PEP"]
 results = []
 
-for symbol in symbols:
-    print(f"Fetching data for {symbol}")
-    response = requests.get(url.format(symbol, api_key))
-    results.append(response.json())
+# Add tasks to the list
+def get_task(session):
+    task = []
+    for symbol in symbols:
+        task.append(session.get(url.format(symbol, api_key), ssl=False))
+        return task
 
-print(results)
+
+# main funciton run the tasks
+async def get_symbols():
+    async with aiohttp.ClientSession() as session:
+        tasks = get_task(session)
+        responses = await asyncio.gather(*tasks)
+        for response in responses:
+            results.append(await response.json())
+
+
+# Event Loop
+asyncio.run(get_symbols())
