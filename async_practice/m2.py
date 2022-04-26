e21d3f0a1fba937c275a45f80fe3dbe64e1e441e
whatiskeptiname <susangmree@gmail.com>
2022-04-26 13:34:39 +0545

    asyncio.gather

--- async_practice/m2.py
+++ async_practice/m2.py
@@ -1,15 +1,24 @@
 import asyncio
-import datetime
 
 
-async def display_date():
-    loop = asyncio.get_running_loop()
-    end_time = loop.time() + 5.0
-    while True:
-        print(datetime.datetime.now())
-        if (loop.time() + 1.0) >= end_time:
-            break
+async def factorial(name, number):
+    f = 1
+    for i in range(2, number + 1):
+        print(f"Task {name}: Compute factorial({number}), currently i={i}...")
         await asyncio.sleep(1)
+        f *= i
+    print(f"Task {name}: factorial({number}) = {f}")
+    return f
 
 
-asyncio.run(display_date())
+async def main():
+    # Schedule three calls *concurrently*:
+    L = await asyncio.gather(
+        factorial("A", 2),
+        factorial("B", 3),
+        factorial("C", 4),
+    )
+    print(L)
+
+
+asyncio.run(main())
