7fea532feaa728ac89a39d6cde066ee41bf48c23
whatiskeptiname <susangmree@gmail.com>
2022-04-26 17:25:16 +0545

    done sync

---
+++ aiohttp_practice/sync_request.py
@@ -0,0 +1,21 @@
+from urllib import response
+import requests
+import os
+import time
+
+# To work with the .env file
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("ALPHAVANTAGE_API_KEY")
+url = "https://www.alphavantage.co/query?function=OVERVIEW&symbol={}&apikey={}"
+symbols = ["AAPL", "GOOG", "TSLA", "MSFT", "PEP", "AAPL", "GOOG", "TSLA", "MSFT", "PEP"]
+results = []
+
+for symbol in symbols:
+    print(f"Fetching data for {symbol}")
+    response = requests.get(url.format(symbol, api_key))
+    results.append(response.json())
+
+print(results)
