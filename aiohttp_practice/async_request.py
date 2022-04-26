2d59fd7b4ff8f54ea061ec28207a2afaa39e9e6c
whatiskeptiname <susangmree@gmail.com>
2022-04-26 17:53:33 +0545

    completed

--- aiohttp_practice/async_request.py
+++ aiohttp_practice/async_request.py
@@ -6,16 +6,51 @@ import os
 
 api_key = os.getenv("ALPHAVANTAGE_API_KEY")
 url = "https://www.alphavantage.co/query?function=OVERVIEW&symbol={}&apikey={}"
-symbols = ["AAPL", "GOOG", "TSLA", "MSFT", "PEP", "AAPL", "GOOG", "TSLA", "MSFT", "PEP"]
+symbols = [
+    "AAPL",
+    "GOOG",
+    "TSLA",
+    "MSFT",
+    "PEP",
+    "AAPL",
+    "GOOG",
+    "TSLA",
+    "MSFT",
+    "PEP",
+    "AAPL",
+    "GOOG",
+    "TSLA",
+    "MSFT",
+    "PEP",
+    "AAPL",
+    "GOOG",
+    "TSLA",
+    "MSFT",
+    "PEP",
+    "AAPL",
+    "GOOG",
+    "TSLA",
+    "MSFT",
+    "PEP",
+    "AAPL",
+    "GOOG",
+    "TSLA",
+    "MSFT",
+    "PEP",
+]
 results = []
 
 # Add tasks to the list
 def get_task(session):
-    task = []
+    tasks = []
     for symbol in symbols:
-        task.append(session.get(url.format(symbol, api_key), ssl=False))
-        return task
+        tasks.append(
+            asyncio.create_task(session.get(url.format(symbol, api_key), ssl=False))
+        )
+        return tasks
 
+
+print(tasks)
 
 # main funciton run the tasks
 async def get_symbols():
@@ -28,3 +63,4 @@ async def get_symbols():
 
 # Event Loop
 asyncio.run(get_symbols())
+print(results)
--- aiohttp_practice/sync_request.py
+++ aiohttp_practice/sync_request.py
@@ -10,7 +10,38 @@ load_dotenv()
 
 api_key = os.getenv("ALPHAVANTAGE_API_KEY")
 url = "https://www.alphavantage.co/query?function=OVERVIEW&symbol={}&apikey={}"
-symbols = ["AAPL", "GOOG", "TSLA", "MSFT", "PEP", "AAPL", "GOOG", "TSLA", "MSFT", "PEP"]
+symbols = [
+    "AAPL",
+    "GOOG",
+    "TSLA",
+    "MSFT",
+    "PEP",
+    "AAPL",
+    "GOOG",
+    "TSLA",
+    "MSFT",
+    "PEP",
+    "AAPL",
+    "GOOG",
+    "TSLA",
+    "MSFT",
+    "PEP",
+    "AAPL",
+    "GOOG",
+    "TSLA",
+    "MSFT",
+    "PEP",
+    "AAPL",
+    "GOOG",
+    "TSLA",
+    "MSFT",
+    "PEP",
+    "AAPL",
+    "GOOG",
+    "TSLA",
+    "MSFT",
+    "PEP",
+]
 results = []
 
 for symbol in symbols:
