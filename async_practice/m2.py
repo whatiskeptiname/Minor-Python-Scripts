a8a4237813e7be722c5cc4af2798f800b2303bc1
whatiskeptiname <susangmree@gmail.com>
2022-04-26 14:14:20 +0545

    task.cancle()

--- async_practice/m2.py
+++ async_practice/m2.py
@@ -1,21 +1,31 @@
 import asyncio
-import time
 
 
-def blocking_io():
-    print(f"start blocking_io at {time.strftime('%X')}")
-    # Note that time.sleep() can be replaced with any blocking
-    # IO-bound operation, such as file operations.
-    time.sleep(1)
-    print(f"blocking_io complete at {time.strftime('%X')}")
+async def cancel_me():
+    print("cancel_me(): before sleep")
+
+    try:
+        # Wait for 1 hour
+        await asyncio.sleep(3600)
+    except asyncio.CancelledError:
+        print("cancel_me(): cancel sleep")
+        raise
+    finally:
+        print("cancel_me(): after sleep")
 
 
 async def main():
-    print(f"started main at {time.strftime('%X')}")
+    # Create a "cancel_me" Task
+    task = asyncio.create_task(cancel_me())
 
-    await asyncio.gather(asyncio.to_thread(blocking_io), asyncio.sleep(1))
+    # Wait for 1 second
+    await asyncio.sleep(1)
 
-    print(f"finished main at {time.strftime('%X')}")
+    task.cancel()
+    try:
+        await task
+    except asyncio.CancelledError:
+        print("main(): cancel_me is cancelled now")
 
 
 asyncio.run(main())
