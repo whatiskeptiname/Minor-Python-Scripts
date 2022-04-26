5ebc85907a14c8df7d59d801b3fc9c5b9831357b
whatiskeptiname <susangmree@gmail.com>
2022-04-26 13:25:27 +0545

    sleeping

--- async_practice/m2.py
+++ async_practice/m2.py
@@ -1,25 +1,15 @@
 import asyncio
-import time
+import datetime
 
 
-async def say_after(delay, what):
-    await asyncio.sleep(delay)
-    print(what)
+async def display_date():
+    loop = asyncio.get_running_loop()
+    end_time = loop.time() + 5.0
+    while True:
+        print(datetime.datetime.now())
+        if (loop.time() + 1.0) >= end_time:
+            break
+        await asyncio.sleep(1)
 
 
-async def main():
-    task1 = asyncio.create_task(say_after(1, "hello"))
-
-    task2 = asyncio.create_task(say_after(2, "world"))
-
-    print(f"started at {time.strftime('%X')}")
-
-    # Wait until both tasks are completed (should take
-    # around 2 seconds.)
-    await task1
-    await task2
-
-    print(f"finished at {time.strftime('%X')}")
-
-
-asyncio.run(main())
+asyncio.run(display_date())
