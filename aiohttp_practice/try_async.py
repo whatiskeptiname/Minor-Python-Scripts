be932d5ddb8b9bbaae2f5f0103d345ee996c75ad
whatiskeptiname <susangmree@gmail.com>
2022-04-27 16:04:12 +0545

    tried multiple exceptions

--- aiohttp_practice/try_async.py
+++ aiohttp_practice/try_async.py
@@ -5,13 +5,22 @@ async def good():
     return "OK"
 
 
-async def bad():
+async def value_error():
     raise ValueError()
 
 
+async def connection_error():
+    raise ConnectionError()
+
+
 async def main():
-    responses = await asyncio.gather(bad(), good(), bad(), return_exceptions=True)
+    responses = await asyncio.gather(good(), connection_error())
     print(responses)
 
 
-asyncio.run(main())
+try:
+    asyncio.run(main())
+except ValueError:
+    print("ValueError")
+except ConnectionError:
+    print("ConnectionError")
