62878dd2c52be542b6b6ac0f6c4a98621bd367ec
whatiskeptiname <susangmree@gmail.com>
2022-04-27 16:21:33 +0545

    excepitons in asyncio.gather

--- aiohttp_practice/try_async.py
+++ aiohttp_practice/try_async.py
@@ -2,37 +2,24 @@ import asyncio
 
 
 async def foo():
-    raise ValueError("Foo value error")
-    return "Foo finished"
+    raise ValueError("Foo value error.")
 
 
 async def bar():
-    await asyncio.sleep(1)
-    return "Bar finished"
+    try:
+        await asyncio.sleep(1)
+        return "Bar finished."
+    except asyncio.CancelledError:
+        print("Bar Cancelled.")
 
 
 async def main():
-    foo_task = asyncio.create_task(foo(), name="Exception_task")
-    bar_task = asyncio.create_task(bar(), name="Waiting_task")
+
     try:
-        done, pending = await asyncio.wait(
-            [foo_task, bar_task], return_when=asyncio.ALL_COMPLETED
-        )
-        for task in done:
-            name = task.get_name()
-            print(f"DONE: {name}")
-            exception = task.exception()
-            if isinstance(exception, Exception):
-                print(f"{name} threw {exception}")
-            try:
-                result = task.result()
-                print(f"{name} returned {result}")
-            except ValueError as e:
-                print(f"ValueError: {e}")
-        for task in pending:
-            task.cancel()
-    except Exception as e:
-        print("Outer Exception")
+        results = await asyncio.gather(foo(), bar(), return_exceptions=False)
+        print(results)
+    except ValueError as e:
+        print(e)
 
 
 asyncio.run(main())
