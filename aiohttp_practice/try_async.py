72edd848f142dcca2721de3aaac44d1d5b940a36
whatiskeptiname <susangmree@gmail.com>
2022-04-27 16:13:42 +0545

    capturing exceptions in task

--- aiohttp_practice/try_async.py
+++ aiohttp_practice/try_async.py
@@ -1,26 +1,30 @@
 import asyncio
 
 
-async def good():
-    return "OK"
+async def foo():
+    raise ValueError("Foo value error")
+    return "Foo finished"
 
 
-async def value_error():
-    raise ValueError()
-
-
-async def connection_error():
-    raise ConnectionError()
+async def bar():
+    await asyncio.sleep(1)
+    return "Bar finished"
 
 
 async def main():
-    responses = await asyncio.gather(good(), connection_error())
-    print(responses)
+    foo_task = asyncio.create_task(foo(), name="Exception task")
+    bar_task = asyncio.create_task(bar(), name="Waiting task")
+    try:
+        done, pending = await asyncio.wait(
+            [foo_task, bar_task], return_when=asyncio.ALL_COMPLETED
+        )
+        for task in done:
+            name = task.get_name()
+            print(f"Done: {name}")
+        for task in pending:
+            task.cancel()
+    except Exception as e:
+        print(f"Exception caught: {e}")
 
 
-try:
-    asyncio.run(main())
-except ValueError:
-    print("ValueError")
-except ConnectionError:
-    print("ConnectionError")
+asyncio.run(main())
