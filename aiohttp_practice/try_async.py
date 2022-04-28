76b509686ae0dc3d85a6e6f9c65f24ff9a545984
whatiskeptiname <susangmree@gmail.com>
2022-04-28 12:02:00 +0545

    retrived error parms

--- aiohttp_practice/try_async.py
+++ aiohttp_practice/try_async.py
@@ -11,27 +11,37 @@ async def foo():
 
 
 async def faa():
+    await asyncio.sleep(1)
     raise SystemError
 
 
-async def bar():
+async def bar(text):
+    text = text
     try:
         await asyncio.sleep(1)
-        return "Bar finished."
-    except asyncio.CancelledError:
+        await faa()
+        return text, "completed:" + text
+    except SystemError:
         print("Bar Cancelled.")
+        return text
 
 
 async def main():
-    results = await asyncio.gather(foo(), bar(), faa(), return_exceptions=True)
+    task_foo = asyncio.create_task(foo())
+    task_faa = asyncio.create_task(faa())
+    task_bar = asyncio.create_task(bar(text="hello world"))
+
+    results = await asyncio.gather(
+        *[task_foo, task_faa, task_bar], return_exceptions=True
+    )
     print(results)
     failed_task = [result for result in results if isinstance(result, Exception)]
-    print(failed_task)
     for error in failed_task:
         if isinstance(error, ValueError):
             print("ValueError!!!!!!!!")
         elif isinstance(error, SystemError):
-            print("SystemError!!!!!!!!")
+            parms = results[2]
+            print("parms:", parms)
 
 
 asyncio.run(main())
